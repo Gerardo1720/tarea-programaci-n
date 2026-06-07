c0163fcd7d0557f18be9eef0085088df76635f09
Gerardo <ggerardosolorzano@gmail.com>
2026-06-07 12:04:08 -0600

    Sistema de calificaciones con reto

---
+++ Examen/Sistema.java
@@ -0,0 +1,43 @@
+package Examen;
+
+import java.util.Scanner;
+
+public class Sistema {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("========== Sistema de Calificaciones ==========");
+        System.out.print("Ingrese su nombre: ");
+        String nombre = teclado.nextLine();
+
+        System.out.print("Ingrese su nota: ");
+        double nota = teclado.nextDouble();
+
+        String resultado = "";
+        String clasificacion = "";
+
+        if (nota >= 70) {
+            resultado = "Aprobo";
+        } else {
+            resultado = "Reprobo";
+        }
+        
+        if (nota >= 90) {
+            clasificacion = "Excelente";
+        } else if (nota >= 80) {
+            clasificacion = "Bueno";
+        } else if (nota >= 70) {
+            clasificacion = "Regular";
+        } else {
+            clasificacion = "Reprobado";
+        }
+
+        System.out.println(" ========== Resultado Final ========== ");
+        System.out.println("Estudiante:    " + nombre);
+        System.out.println("Nota:          " + nota);
+        System.out.println("Resultado:     " + resultado);
+        System.out.println("Clasificacion: " + clasificacion);
+        teclado.close();
+    }
+}
